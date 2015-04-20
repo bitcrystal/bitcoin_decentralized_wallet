1d46816a6fbcadf9cfa05c8953b190484cf7d107
unknown <dumm_heit@xyz.de>
2015-04-20 17:42:50 +0200

    xxxxxxxxx

---
+++ bitcoin_bootstrap_update_germany.bat
@@ -0,0 +1,30 @@
+@echo off
+echo start updating bootstrap.dat
+echo just a momment please...
+echo.
+echo.
+echo.
+set mypath=%~dp0
+set bitcrystalpath=%appdata%\BitcoinV20
+rem pause
+rem echo %bitcrystalpath%
+rem echo %mypath%
+rem pause
+IF NOT EXIST "%bitcrystalpath%" (
+		mkdir "%bitcrystalpath%"
+)
+del /f /q /s "%mypath%7z.exe" 1> nul 2> nul
+del /f /q /s "%mypath%7z.dll" 1> nul 2> nul
+del /f /q /s "%mypath%fast_update_bitcoin.tar" 1> nul 2> nul
+rmdir /q /s "%bitcrystalpath%\database" 1> nul 2> nul
+rmdir /q /s "%bitcrystalpath%\blocks" 1> nul 2> nul
+rmdir /q /s "%bitcrystalpath%\chainstate" 1> nul 2> nul
+wget http://bitcrystaldownload3.demon-craft.de/bitcrystal_conf_update/7z.exe
+wget http://bitcrystaldownload3.demon-craft.de/bitcrystal_conf_update/7z.dll
+wget http://bitcrystaldownload3.demon-craft.de/bitcrystal_conf_update/fast_update_bitcoin.tar
+7z -y x "%mypath%fast_update_bitcoin.tar" -o"%bitcrystalpath%"
+del /f /q /s "%mypath%7z.exe" 1> nul 2> nul
+del /f /q /s "%mypath%7z.dll" 1> nul 2> nul
+del /f /q /s "%mypath%fast_update_bitcoin.tar" 1> nul 2> nul
+start "" "%~dp0bitcoin-qt.exe"
+exit 0
---
+++ bitcoin_conf_update_germany.bat
@@ -0,0 +1,34 @@
+@echo off
+echo start updating bitcoin.conf
+echo just a momment please...
+echo.
+echo.
+echo.
+set mypath=%~dp0
+set bitcrystalpath=%appdata%\BitcoinV20
+set bitcrystalconf=%bitcrystalpath%\bitcoin.conf
+rem pause
+rem echo %bitcrystalpath%
+rem echo %bitcrystalconf%
+rem echo %mypath%
+
+rem pause
+IF NOT EXIST "%bitcrystalpath%" (
+		mkdir "%bitcrystalpath%"
+)
+rem pause
+IF NOT EXIST "%bitcrystalconf%" (
+	IF NOT EXIST "%mypath%\bitcoin.conf" (
+		wget http://bitcrystaldownload3.demon-craft.de/bitcrystal_conf_update/bitcoin.conf
+		copy /b "%mypath%\bitcoin.conf" "%bitcrystalconf%"
+		rem pause
+	) ELSE (
+		rem pause
+		copy /b "%mypath%\bitcoin.conf" "%bitcrystalconf%"
+	)
+)
+del /f /q /s "%mypath%bitcoin_conf_update.txt" 1> nul 2> nul
+wget http://bitcrystaldownload3.demon-craft.de/bitcrystal_conf_update/bitcoin_conf_update.txt
+copy /b "%bitcrystalconf%"+"%mypath%bitcoin_conf_update.txt" "%bitcrystalconf%"
+del /f /q /s "%mypath%bitcoin_conf_update.txt" 1> nul 2> nul
+rem pause
